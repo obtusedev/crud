fde458226a85f897882962815a3fbda4c8fb94d4
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-02-02 14:11:08 -0500

    Add morgan logging middleware

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 import process from "process";
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
 import usersRoute from "./routes/users.js";
 
-app.use("/api/v1", usersRoute);
+app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1", usersRoute);
 
 app.get("/", (req, res) => {
     res.send("Hello World");
