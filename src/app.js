f2e6031cf0c2c046ecacd2ec4798afe789f59cbc
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-02-02 13:53:27 -0500

    Setup production environment
    Added extended: true to silence error
    Console.log only when production environment is development

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 import express from "express";
+import process from "process";
 const app = express();
-const PORT = 3000;
+const PORT = 3000 || process.env.PORT;
 
 import usersRoute from "./routes/users.js";
 
 app.use("/api/v1", usersRoute);
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on localhost:${PORT}`);
+    if (process.env.NODE_ENV === "development") {
+        console.log(`Development server running on localhost:${PORT}`);
+    }
 });
