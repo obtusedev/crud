d1c465f0e79dfee4058f4f7c8eb4cad7e8354160
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-02-04 19:35:51 -0500

    Add User data validation via Joi

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import process from "process";
+import Joi from "joi";
 import Database from "better-sqlite3";
 const options = {
     verbose: process.env.NODE_ENV === "development" ? console.log : null,
@@ -6,6 +7,63 @@ const options = {
 };
 const db = new Database("./User.sqlite3", options);
 
+export const userSchema = Joi.object({
+    first_name: Joi.string()
+        .min(1)
+        .max(25)
+        .required()
+        .description("Your first name")
+        .example("John"),
+    last_name: Joi.string()
+        .min(1)
+        .max(25)
+        .required()
+        .description("Your last name")
+        .example("Doe"),
+    email: Joi.string()
+        .email()
+        .required()
+        .description("Your email")
+        .example("admin@example.com"),
+    gender: Joi.string()
+        .min(4)
+        .max(6)
+        .allow("Male", "Female")
+        .required()
+        .description("Your gender")
+        .example("Male, Female"),
+    user_agent: Joi.string()
+        .min(10)
+        .max(50)
+        .allow("")
+        .default("")
+        .required()
+        .example(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
+        ),
+    user_avatar: Joi.string()
+        .min(3)
+        .max(50)
+        .allow("")
+        .default("")
+        .description("url to avatar image")
+        .example("www.example.com/path/to/img.jpg"),
+    city: Joi.string()
+        .min(1)
+        .max(25)
+        .allow("")
+        .required()
+        .description("Your city")
+        .example("Chicago"),
+    country: Joi.string()
+        .min(1)
+        .max(50)
+        .allow("")
+        .default("United States")
+        .required()
+        .description("Your country")
+        .example("United States"),
+});
 const allFields = `id, first_name, last_name, email, gender, user_agent, user_avatar, city, country`;
 
 export function getUsers(count) {
