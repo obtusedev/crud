bf041d1bf18056e41ab2aef29cd4a59e4e59f434
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-02-07 18:45:14 -0500

    Add helmet for added security

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 import express from "express";
 import morgan from "morgan";
-import process, { env } from "process";
+import helmet from "helmet";
+import process from "process";
 const app = express();
 const PORT = 3000 || process.env.PORT;
 const environment = process.env.NODE_ENV;
 
 import usersRoute from "./routes/users.js";
 
-environment == "development" ? app.use(morgan("dev")) : app.use(morgan("combined"));
+environment == "development"
+    ? app.use(morgan("dev"))
+    : app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
 app.use("/api/v1", usersRoute);
 
 app.get("/", (req, res) => {
