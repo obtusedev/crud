0c6be262601d527eab1d1114dd7fc4be7bbfe405
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-02-09 18:04:04 -0500

    Update response data for consistency
    Response payload inspiration taken from Spotify api

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,102 @@
+import * as User from "../models/User.js";
+
+export function getUsers(req, res) {
+    const limit = parseInt(req.query.limit) || 5; // return 5 users by default
+    try {
+        const users = User.getUsers(limit);
+        res.status(200).json({
+            status: 200,
+            users: users,
+        });
+    } catch (e) {
+        res.status(500).json({
+            error: {
+                status: 500,
+                message: "Internal Server Error",
+            },
+        });
+    }
+}
+
+export function getUserById(req, res) {
+    const id = req.params.id;
+
+    try {
+        const user = User.getUserById(id);
+        if (user === undefined) {
+            res.status(404).json({
+                error: {
+                    status: 404,
+                    message: "Not Found",
+                },
+            });
+        } else if (user !== undefined) {
+            res.status(200).json({
+                status: 200,
+                user: user,
+            });
+        }
+    } catch (e) {
+        res.status(500).json({
+            error: {
+                status: 500,
+                message: "Internal Server Error",
+            },
+        });
+    }
+}
+
+export function createUser(req, res) {
+    const validation = User.userSchema.validate(req.body, {
+        abortEarly: false, // show all errors
+    });
+    let { error } = validation;
+    if (error !== undefined) {
+        let { details } = error;
+        res.status(400).json({
+            status: 400,
+            message: details,
+        });
+    } else {
+        let userCreated = User.createUser(req.body);
+        if (userCreated) {
+            res.status(200).json({
+                status: 200,
+                message: "OK",
+            });
+        } else {
+            res.status(400).json({
+                status: 400,
+                message: "Bad Request",
+            });
+        }
+    }
+}
+
+export function updateUser(req, res) {
+    // TODO: implement
+}
+
+export function deleteUser(req, res) {
+    const id = req.params.id;
+
+    try {
+        const userDeleted = User.deleteUser(id);
+        if (userDeleted) {
+            res.status(200).json({
+                status: 200,
+                message: "OK",
+            });
+        } else {
+            res.status(404).json({
+                status: 404,
+                message: "Not Found",
+            });
+        }
+    } catch (e) {
+        res.status(500).json({
+            status: 500,
+            message: "Internal Server Error",
+        });
+    }
+}
